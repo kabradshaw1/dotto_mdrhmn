f8e3658d6eca6d81c6cad01bc507f82d8d185b55
kabradshaw1 <itllbok82@gmail.com>
2023-01-27 12:49:24 -0500

    meh

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  username = models.CharField(max_length=15)
+  password = models.CharField(max_length=15)
+
+  def __str__(self):
+    return self.username
